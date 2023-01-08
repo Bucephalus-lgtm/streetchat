5ab9773424335d1664493d22779f9eadfc3cf573
Bhargab Nath <bhargabnath691@gmail.com>
2023-01-08 11:50:45 +0530

    Pages of HomePage and ChatPage added in frontend folder

---
+++ frontend/src/Pages/Chatpage.js
@@ -0,0 +1,11 @@
+const Chatpage = () => {
+  return (
+    <div style={{ width: "100%" }}>
+      {/* Sidedrawer */}
+      {/* My chats */}
+      {/* Chatbox */}
+    </div>
+  );
+};
+
+export default Chatpage;
---
+++ frontend/src/Pages/Homepage.js
@@ -0,0 +1,9 @@
+function Homepage() {
+  return (
+    <Container maxW="xl" centerContent>
+      <h1>Home Page</h1>
+    </Container>
+  );
+}
+
+export default Homepage;
